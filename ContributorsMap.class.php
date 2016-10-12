10cb1db05a2af0ca6e4a27d413031e9ca6b4e40f
Riccardo Iaconelli <riccardo@kde.org>
2016-10-12 18:26:18 +0200

    protocol relative embed
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- ContributorsMap.class.php
+++ ContributorsMap.class.php
@@ -8,7 +8,7 @@ class ContributorsMap {
 
     public static function renderContributorsMap( $input, array $args, Parser $parser, PPFrame $frame ) {
 
-       $output = '<iframe width="100%" height="300px" frameBorder="0" src="http://umap.openstreetmap.fr/en/map/wikitolearn-community_106733?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false"></iframe><p><a href="http://umap.openstreetmap.fr/en/map/wikitolearn-community_106733">See full screen</a></p>';
+       $output = '<iframe width="100%" height="300px" frameBorder="0" src="//umap.openstreetmap.fr/en/map/wikitolearn-community_106733?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=null&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=null&onLoadPanel=undefined&captionBar=false"></iframe><p><a href="//umap.openstreetmap.fr/en/map/wikitolearn-community_106733">See full screen</a></p>';
 
        return $output;
     }
