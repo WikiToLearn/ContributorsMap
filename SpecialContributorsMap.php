84a56aec34c693e44ebe21193a38f27d7e1a0d22
Riccardo Iaconelli <riccardo@kde.org>
2016-10-14 12:26:28 +0200

    default configuration to empty value, error if at least the map is not set
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- SpecialContributorsMap.php
+++ SpecialContributorsMap.php
@@ -1,26 +1,31 @@
 <?php
 
-class SpecialContributorsMap extends IncludableSpecialPage {
-
-  	function __construct() {
-  		parent::__construct( 'ContributorsMap');
-  	}
-
-    function execute( $par ) {
-      global $wgCMURL;
-      global $wgCMDisplay;
-
-  	   $out = $this->getOutput();
-
-       $params = array();
-       foreach($wgCMDisplay as $x => $x_value) {
-          $params[] = $x . "=" . $x_value;
-        }
-
-       $output = '<iframe width="100%" height="300px" frameBorder="0" src="//'.$wgCMURL.'?'.implode('&', $params).'"></iframe>';
-      //  $output .= '<p><a href="//'.$wgCMURL.">See full screen</a></p>';
-
-       $out->addHTML($output);
+class SpecialContributorsMap extends IncludableSpecialPage
+{
+    public function __construct()
+    {
+        parent::__construct('ContributorsMap');
     }
 
+    public function execute($par)
+    {
+        global $wgCMURL;
+        global $wgCMDisplay;
+
+        $out = $this->getOutput();
+
+        $params = array();
+        foreach ($wgCMDisplay as $x => $x_value) {
+            $params[] = $x.'='.urlencode($x_value);
+        }
+
+        $output = '<iframe width="100%" height="300px" frameBorder="0" src="//'.$wgCMURL.'?'.implode('&', $params).'"></iframe>';
+      //  $output .= '<p><a href="//'.$wgCMURL.">See full screen</a></p>';
+
+       if ( $wgCMURL === "" ) {
+         $out->addwikitext('Please set <code>$wgCMURL</code> in <code>LocalSettings.php</code> to some meaningful value!');
+       } else {
+         $out->addHTML($output);
+       }
+    }
 }
