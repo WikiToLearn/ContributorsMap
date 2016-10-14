2ae4363b235865c729360f10591fbcb8c0ce2e1a
Riccardo Iaconelli <riccardo@kde.org>
2016-10-14 12:06:56 +0200

    make the extension configurable
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- SpecialContributorsMap.php
+++ SpecialContributorsMap.php
@@ -1,33 +1,26 @@
 <?php
 
-
 class SpecialContributorsMap extends IncludableSpecialPage {
 
   	function __construct() {
-  		parent::__construct( 'ContributorsMap' );
+  		parent::__construct( 'ContributorsMap');
   	}
 
     function execute( $par ) {
-  	   $out = $this->getOutput();
-       $arr = array ();
-       $arr[] = "scaleControl=false";
-       $arr[] = "miniMap=false";
-       $arr[] = "scrollWheelZoom=false";
-       $arr[] = "zoomControl=true";
-       $arr[] = "allowEdit=false";
-       $arr[] = "moreControl=null";
-       $arr[] = "searchControl=null";
-       $arr[] = "tilelayersControl=null";
-       $arr[] = "embedControl=null";
-       $arr[] = "datalayersControl=null";
-       $arr[] = "onLoadPanel=undefined";
-       $arr[] = "captionBar=false";
+      global $wgCMURL;
+      global $wgCMDisplay;
 
-       $output = '<iframe width="100%" height="300px" frameBorder="0" src="//umap.openstreetmap.fr/en/map/wikitolearn-community_106733?'.implode('&', $arr).'"></iframe>';
-       $output .= '<p><a href="//umap.openstreetmap.fr/en/map/wikitolearn-community_106733">See full screen</a></p>';
+  	   $out = $this->getOutput();
+
+       $params = array();
+       foreach($wgCMDisplay as $x => $x_value) {
+          $params[] = $x . "=" . $x_value;
+        }
+
+       $output = '<iframe width="100%" height="300px" frameBorder="0" src="//'.$wgCMURL.'?'.implode('&', $params).'"></iframe>';
+      //  $output .= '<p><a href="//'.$wgCMURL.">See full screen</a></p>';
 
        $out->addHTML($output);
-       //$out->addwikitext($output);
     }
 
 }
